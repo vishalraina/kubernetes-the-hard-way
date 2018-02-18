fcd9e25e4b58e481c340557830c3e2635db4f889
Vishal <vishalraina@users.noreply.github.com>
2018-02-17 22:42:02 -0500

    updating Kubernetes API Server systemd config

--- scripts/bootstrapping-kubernetes-controllers.sh
+++ scripts/bootstrapping-kubernetes-controllers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+set -x
 #Run this script in debug mode
 
 #Bash script based on steps mentioned in 
@@ -22,32 +23,59 @@ chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
 #Install the Kubernetes binaries
 sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
 
+ls -ltr /usr/local/bin/kube-apiserver 
+ls -ltr /usr/local/bin/kube-controller-manager 
+ls -ltr /usr/local/bin/kube-scheduler 
+ls -ltr /usr/local/bin/kubectl
+
 #Configure the Kubernetes API Server
 sudo mkdir -p /var/lib/kubernetes/
 sudo mv ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem encryption-config.yaml /var/lib/kubernetes/
+ls -ltr /var/lib/kubernetes/
 
 #Get internal IP
 INTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" \
   http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
   
  #Create the kube-apiserver.service systemd unit file
-cat > kube-controller-manager.service <<EOF
+
+cat > kube-apiserver.service <<EOF
 [Unit]
-Description=Kubernetes Controller Manager
+Description=Kubernetes API Server
 Documentation=https://github.com/kubernetes/kubernetes
 
 [Service]
-ExecStart=/usr/local/bin/kube-controller-manager \\
-  --address=0.0.0.0 \\
-  --cluster-cidr=10.200.0.0/16 \\
-  --cluster-name=kubernetes \\
-  --cluster-signing-cert-file=/var/lib/kubernetes/ca.pem \\
-  --cluster-signing-key-file=/var/lib/kubernetes/ca-key.pem \\
-  --leader-elect=true \\
-  --master=http://127.0.0.1:8080 \\
-  --root-ca-file=/var/lib/kubernetes/ca.pem \\
-  --service-account-private-key-file=/var/lib/kubernetes/ca-key.pem \\
+ExecStart=/usr/local/bin/kube-apiserver \\
+  --admission-control=Initializers,NamespaceLifecycle,NodeRestriction,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota \\
+  --advertise-address=${INTERNAL_IP} \\
+  --allow-privileged=true \\
+  --apiserver-count=3 \\
+  --audit-log-maxage=30 \\
+  --audit-log-maxbackup=3 \\
+  --audit-log-maxsize=100 \\
+  --audit-log-path=/var/log/audit.log \\
+  --authorization-mode=Node,RBAC \\
+  --bind-address=0.0.0.0 \\
+  --client-ca-file=/var/lib/kubernetes/ca.pem \\
+  --enable-swagger-ui=true \\
+  --etcd-cafile=/var/lib/kubernetes/ca.pem \\
+  --etcd-certfile=/var/lib/kubernetes/kubernetes.pem \\
+  --etcd-keyfile=/var/lib/kubernetes/kubernetes-key.pem \\
+  --etcd-servers=https://10.240.0.10:2379,https://10.240.0.11:2379,https://10.240.0.12:2379 \\
+  --event-ttl=1h \\
+  --experimental-encryption-provider-config=/var/lib/kubernetes/encryption-config.yaml \\
+  --insecure-bind-address=127.0.0.1 \\
+  --kubelet-certificate-authority=/var/lib/kubernetes/ca.pem \\
+  --kubelet-client-certificate=/var/lib/kubernetes/kubernetes.pem \\
+  --kubelet-client-key=/var/lib/kubernetes/kubernetes-key.pem \\
+  --kubelet-https=true \\
+  --runtime-config=api/all \\
+  --service-account-key-file=/var/lib/kubernetes/ca-key.pem \\
   --service-cluster-ip-range=10.32.0.0/24 \\
+  --service-node-port-range=30000-32767 \\
+  --tls-ca-file=/var/lib/kubernetes/ca.pem \\
+  --tls-cert-file=/var/lib/kubernetes/kubernetes.pem \\
+  --tls-private-key-file=/var/lib/kubernetes/kubernetes-key.pem \\
   --v=2
 Restart=on-failure
 RestartSec=5
@@ -103,6 +131,9 @@ WantedBy=multi-user.target
 EOF
 
 #Deploy reload and start Controller Services
+ls -ltr  /etc/systemd/system/
+sleep 2
+echo "I am sleeping to 2 seconds"
 sudo mv kube-apiserver.service kube-scheduler.service kube-controller-manager.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
